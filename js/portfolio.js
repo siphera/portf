ead026709338e8d42ce33b23be4f369dcac47fd1
siphera <salmansiphenkosi@gmail.com>
2018-05-27 05:27:17 +0200

    created photo animation

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,17 +1,30 @@
-document.addEventListener('DOMContentLoaded', function(){
+//document.addEventListener('DOMContentLoaded', function(){
+//    
+//const tabs = document.querySelector('.tabs');
+//  const panels = document.querySelectorAll('.panel');
+//  tabs.addEventListener('click', (e) => {
+//    if(e.target.tagName == 'LI'){
+//      const targetPanel = document.querySelector(e.target.dataset.target);
+//      Array.from(panels).forEach((panel) => {
+//        if(panel == targetPanel){
+//          panel.classList.add('active');
+//        }else{
+//          panel.classList.remove('active');
+//        }
+//      });
+//    }
+//  });
+//});
+
+$(document).ready(function(){
     
-const tabs = document.querySelector('.tabs');
-  const panels = document.querySelectorAll('.panel');
-  tabs.addEventListener('click', (e) => {
-    if(e.target.tagName == 'LI'){
-      const targetPanel = document.querySelector(e.target.dataset.target);
-      Array.from(panels).forEach((panel) => {
-        if(panel == targetPanel){
-          panel.classList.add('active');
-        }else{
-          panel.classList.remove('active');
+    $(window).scroll(function(event){
+        
+        var y = $(this).scrollTop();
+        
+        if (y >= 0) {
+            $('#photo1').addClass('animate');
+            $('#photo3').addClass('animate');
         }
-      });
-    }
-  });
+    });
 });
